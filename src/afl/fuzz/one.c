9d813574204a31af6ec4827642c6ac070dcf24a9
Firas Zaidan <firas@zaidan.de>
2018-11-25 21:19:32 +0100

    Refactor `fuzz/one.c` to extract `stage_flip4`

--- src/afl/fuzz/one.c
+++ src/afl/fuzz/one.c
@@ -11,6 +11,7 @@
 #include "afl/mutate/eff.h"
 #include "afl/mutate/stage/flip1.h"
 #include "afl/mutate/stage/flip2.h"
+#include "afl/mutate/stage/flip4.h"
 #include "afl/fuzz/common.h"
 #include "afl/fuzz/stages.h"
 #include "afl/queue_entry.h"
@@ -485,35 +486,9 @@ u8 fuzz_one(char** argv) {
     goto abandon_entry;
   }
 
-  /* Four walking bits. */
-  stage_name  = "bitflip 4/1";
-  stage_short = "flip4";
-  stage_max   = (len << 3) - 3;
-
-  orig_hit_cnt = new_hit_cnt;
-
-  for (stage_cur = 0; stage_cur < stage_max; stage_cur++) {
-    stage_cur_byte = stage_cur >> 3;
-
-    FLIP_BIT(out_buf, stage_cur);
-    FLIP_BIT(out_buf, stage_cur + 1);
-    FLIP_BIT(out_buf, stage_cur + 2);
-    FLIP_BIT(out_buf, stage_cur + 3);
-
-    if (common_fuzz_stuff(argv, out_buf, len)) {
-      goto abandon_entry;
-    }
-
-    FLIP_BIT(out_buf, stage_cur);
-    FLIP_BIT(out_buf, stage_cur + 1);
-    FLIP_BIT(out_buf, stage_cur + 2);
-    FLIP_BIT(out_buf, stage_cur + 3);
+  if(!stage_flip4(argv, &orig_hit_cnt, &new_hit_cnt, out_buf, len)) {
+    goto abandon_entry;
   }
-
-  new_hit_cnt = queued_paths + unique_crashes;
-
-  stage_finds[STAGE_FLIP4]  += new_hit_cnt - orig_hit_cnt;
-  stage_cycles[STAGE_FLIP4] += stage_max;
 
   /* Initialize effector map for the next step (see comments below). Always
      flag first and last byte as doing something. */
