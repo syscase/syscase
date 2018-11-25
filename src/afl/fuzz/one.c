cd36b76da54f5f0271d97d4e1b6bfa3cd3419aaf
Firas Zaidan <firas@zaidan.de>
2018-11-25 21:19:31 +0100

    Refactor `fuzz/one.c` to extract `mutate/eff.h`

--- src/afl/fuzz/one.c
+++ src/afl/fuzz/one.c
@@ -8,6 +8,7 @@
 #include "afl/testcase/result.h"
 #include "afl/testcase/calibrate.h"
 #include "afl/mutate/flip.h"
+#include "afl/mutate/eff.h"
 #include "afl/mutate/stage/flip1.h"
 #include "afl/mutate/stage/flip2.h"
 #include "afl/fuzz/common.h"
@@ -513,18 +514,6 @@ u8 fuzz_one(char** argv) {
 
   stage_finds[STAGE_FLIP4]  += new_hit_cnt - orig_hit_cnt;
   stage_cycles[STAGE_FLIP4] += stage_max;
-
-  /* Effector map setup. These macros calculate:
-
-     EFF_APOS      - position of a particular file offset in the map.
-     EFF_ALEN      - length of a map with a particular number of bytes.
-     EFF_SPAN_ALEN - map span for a sequence of bytes.
-
-   */
-#define EFF_APOS(_p)          ((_p) >> EFF_MAP_SCALE2)
-#define EFF_REM(_x)           ((_x) & ((1 << EFF_MAP_SCALE2) - 1))
-#define EFF_ALEN(_l)          (EFF_APOS(_l) + !!EFF_REM(_l))
-#define EFF_SPAN_ALEN(_p, _l) (EFF_APOS((_p) + (_l) - 1) - EFF_APOS(_p) + 1)
 
   /* Initialize effector map for the next step (see comments below). Always
      flag first and last byte as doing something. */
