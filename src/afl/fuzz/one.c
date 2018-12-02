b6bf667eb4c29f936712cb17bb8a9d26836e9b4b
Firas Zaidan <firas@zaidan.de>
2018-12-02 22:07:28 +0100

    Refactor `fuzz/one.c` to extract `stage_user_extras_u0`

--- src/afl/fuzz/one.c
+++ src/afl/fuzz/one.c
@@ -24,6 +24,7 @@
 #include "afl/mutate/stage/interest8.h"
 #include "afl/mutate/stage/interest16.h"
 #include "afl/mutate/stage/interest32.h"
+#include "afl/mutate/stage/user_extras_u0.h"
 #include "afl/fuzz/common.h"
 #include "afl/fuzz/stages.h"
 #include "afl/queue_entry.h"
@@ -407,57 +408,9 @@ skip_interest:
     goto skip_user_extras;
   }
 
-  /* Overwrite with user-supplied extras. */
-  stage_name  = "user extras (over)";
-  stage_short = "ext_UO";
-  stage_cur   = 0;
-  stage_max   = extras_cnt * len;
-
-  stage_val_type = STAGE_VAL_NONE;
-
-  orig_hit_cnt = new_hit_cnt;
-
-  for (i = 0; i < len; i++) {
-    u32 last_len = 0;
-
-    stage_cur_byte = i;
-
-    /* Extras are sorted by size, from smallest to largest. This means
-       that we don't have to worry about restoring the buffer in
-       between writes at a particular offset determined by the outer
-       loop. */
-    for (j = 0; j < extras_cnt; j++) {
-      /* Skip extras probabilistically if extras_cnt > MAX_DET_EXTRAS. Also
-         skip them if there's no room to insert the payload, if the token
-         is redundant, or if its entire span has no bytes set in the effector
-         map. */
-      if ((extras_cnt > MAX_DET_EXTRAS && UR(extras_cnt) >= MAX_DET_EXTRAS) ||
-          extras[j].len > len - i ||
-          !memcmp(extras[j].data, out_buf + i, extras[j].len) ||
-          !memchr(eff_map + EFF_APOS(i), 1, EFF_SPAN_ALEN(i, extras[j].len))) {
-
-        stage_max--;
-        continue;
-      }
-
-      last_len = extras[j].len;
-      memcpy(out_buf + i, extras[j].data, last_len);
-
-      if (common_fuzz_stuff(argv, out_buf, len)) {
-        goto abandon_entry;
-      }
-
-      stage_cur++;
-    }
-
-    /* Restore all the clobbered memory. */
-    memcpy(out_buf + i, in_buf + i, last_len);
+  if(!stage_user_extras_u0(argv, &orig_hit_cnt, &new_hit_cnt, in_buf, out_buf, len, eff_map)) {
+    goto abandon_entry;
   }
-
-  new_hit_cnt = queued_paths + unique_crashes;
-
-  stage_finds[STAGE_EXTRAS_UO]  += new_hit_cnt - orig_hit_cnt;
-  stage_cycles[STAGE_EXTRAS_UO] += stage_max;
 
   /* Insertion of user-supplied extras. */
   stage_name  = "user extras (insert)";
