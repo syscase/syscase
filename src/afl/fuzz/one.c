d6418df503d22d1c01d088810715d84b9a1f4d35
Firas Zaidan <firas@zaidan.de>
2018-12-02 22:07:19 +0100

    Refactor `fuzz/one.c` to extract `mutate/test/bitflip.c`

--- src/afl/fuzz/one.c
+++ src/afl/fuzz/one.c
@@ -9,6 +9,7 @@
 #include "afl/testcase/calibrate.h"
 #include "afl/mutate/flip.h"
 #include "afl/mutate/eff.h"
+#include "afl/mutate/test/bitflip.h"
 #include "afl/mutate/stage/flip1.h"
 #include "afl/mutate/stage/flip2.h"
 #include "afl/mutate/stage/flip4.h"
@@ -150,43 +151,6 @@ u32 calculate_score(struct queue_entry* q) {
   }
 
   return perf_score;
-}
-
-/* Helper function to see if a particular change (xor_val = old ^ new) could
-   be a product of deterministic bit flips with the lengths and stepovers
-   attempted by afl-fuzz. This is used to avoid dupes in some of the
-   deterministic fuzzing operations that follow bit flips. We also
-   return 1 if xor_val is zero, which implies that the old and attempted new
-   values are identical and the exec would be a waste of time. */
-u8 could_be_bitflip(u32 xor_val) {
-  u32 sh = 0;
-
-  if (!xor_val) {
-    return 1;
-  }
-
-  /* Shift left until first bit set. */
-  while (!(xor_val & 1)) {
-    sh++;
-    xor_val >>= 1;
-  }
-
-  /* 1-, 2-, and 4-bit patterns are OK anywhere. */
-  if (xor_val == 1 || xor_val == 3 || xor_val == 15) {
-    return 1;
-  }
-
-  /* 8-, 16-, and 32-bit patterns are OK only if shift factor is
-     divisible by 8, since that's the stepover for these ops. */
-  if (sh & 7) {
-    return 0;
-  }
-
-  if (xor_val == 0xff || xor_val == 0xffff || xor_val == 0xffffffff) {
-    return 1;
-  }
-
-  return 0;
 }
 
 /* Helper function to see if a particular value is reachable through
