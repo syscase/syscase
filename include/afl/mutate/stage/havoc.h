d460e2207f5e69bc4e715f3a92c26bbc574f8614
Firas Zaidan <firas@zaidan.de>
2018-12-02 22:07:31 +0100

    Refactor `fuzz/one.c` to extract `stage_havoc`

---
+++ include/afl/mutate/stage/havoc.h
@@ -0,0 +1,10 @@
+#ifndef AFL_MUTATE_STAGE_HAVOC_H
+#define AFL_MUTATE_STAGE_HAVOC_H
+
+u32 choose_block_len(u32 limit);
+int stage_havoc(char** argv, u64 *orig_hit_cnt, u64 *new_hit_cnt,
+    u8 *in_buf, u8 *out_buf, s32 len, u8 *eff_map, u32 splice_cycle,
+    u32 orig_perf, u32 *perf_score, u8 doing_det);
+
+#endif /* ! AFL_MUTATE_STAGE_HAVOC_H */
+
