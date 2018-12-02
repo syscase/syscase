94312cff0e61abac48db4c33b0a19cd941fc668a
Firas Zaidan <firas@zaidan.de>
2018-12-02 22:07:30 +0100

    Refactor `fuzz/one.c` to extract `stage_auto_extras_a0`

---
+++ include/afl/mutate/stage/auto_extras_a0.h
@@ -0,0 +1,8 @@
+#ifndef AFL_MUTATE_STAGE_AUTO_EXTRAS_A0_H
+#define AFL_MUTATE_STAGE_AUTO_EXTRAS_A0_H
+
+int stage_auto_extras_a0(char** argv, u64 *orig_hit_cnt, u64 *new_hit_cnt,
+    u8 *in_buf, u8 *out_buf, s32 len, u8 *eff_map);
+
+#endif /* ! AFL_MUTATE_STAGE_AUTO_EXTRAS_A0_H */
+
