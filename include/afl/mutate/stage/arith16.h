fd07ac832b37c33d2e9e1c1dcf886e8ecb62d83f
Firas Zaidan <firas@zaidan.de>
2018-12-02 22:07:23 +0100

    Refactor `fuzz/one.c` to extract `stage_arith16`

---
+++ include/afl/mutate/stage/arith16.h
@@ -0,0 +1,8 @@
+#ifndef AFL_MUTATE_STAGE_ARITH16_H
+#define AFL_MUTATE_STAGE_ARITH16_H
+
+int stage_arith16(char** argv, u64 *orig_hit_cnt, u64 *new_hit_cnt,
+    u8 *out_buf, s32 len, u8 *eff_map);
+
+#endif /* ! AFL_MUTATE_STAGE_ARITH16_H */
+
