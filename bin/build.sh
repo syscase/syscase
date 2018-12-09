761cc27ccf3ce3659872169f2738eb891ccbb58a
Firas Zaidan <firas@zaidan.de>
2018-12-10 00:01:58 +0100

    Move `build.sh` to `bin/build.sh`
    * Add source strict-mode.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-BASE_PATH=$(dirname "$(readlink -f "$0")")
+BASE_PATH=$(dirname "$(dirname "$(readlink -f "$0")")")
+
+source "$BASE_PATH/bin/shared/strict-mode.sh"
 
 BUILD_DIR="$BASE_PATH/build"
 
