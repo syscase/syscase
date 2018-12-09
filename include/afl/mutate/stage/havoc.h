5eb39793af8283f57e7136194d5c592f6199c60a
Firas Zaidan <firas@zaidan.de>
2018-12-09 19:32:50 +0100

    Fix `Corrupted head alloc canary` in `stage_havoc`
    Havoc stage requires reallocation of `in_buf` and `out_buf`. If we do
    not pass the pointer address of `in_buf` and `out_buf`, havoc stage
    will fail with `Corrupted head alloc canary` at `DFL_ck_free()` in
    `include/afl/alloc-inl.h:143`.

--- include/afl/mutate/stage/havoc.h
+++ include/afl/mutate/stage/havoc.h
@@ -3,7 +3,7 @@
 
 u32 choose_block_len(u32 limit);
 int stage_havoc(char** argv, u64 *orig_hit_cnt, u64 *new_hit_cnt,
-    u8 *in_buf, u8 *out_buf, s32 len, u8 *eff_map, u32 splice_cycle,
+    u8 **orig_in_buf, u8 **orig_out_buf, s32 len, u8 *eff_map, u32 splice_cycle,
     u32 orig_perf, u32 *perf_score, u8 doing_det, u8 *orig_in);
 
 #endif /* ! AFL_MUTATE_STAGE_HAVOC_H */
